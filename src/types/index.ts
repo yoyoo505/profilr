73e392e44d04eef4f446909f9f0f9039c5e5b3a5
leyooo <leonardomessisamosir@gmail.com>
2025-03-14 16:35:33 +0700

    index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface SocialLink {
+  id: string;
+  platform: string;
+  url: string;
+  icon: string;
+  color: string;
+}
+
+export interface UserProfile {
+  name: string;
+  bio: string;
+  avatar: string;
+  username: string;
+}
